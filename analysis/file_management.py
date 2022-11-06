6d6a03cdb06b6e7858e12575edd38769e82a0db1
Michal Burgunder <michal_burgunder@yahoo.com>
2022-11-06 17:06:41 +0100

    iontermediary push

--- analysis/file_management.py
+++ analysis/file_management.py
@@ -17,7 +17,7 @@ def write_into_one_csv(root, paths, descriptor, just_headers=False):
     return f"{root}/temp_{descriptor}"
 
 def save_as_csv(root, data, descriptor, headers):
-    with open(f"{root}/temp_{descriptor}.txt", "wt") as fw:
+    with open(f"{root}/temp_{descriptor}.csv", "wt") as fw:
         writer = csv.writer(fw)
         writer.writerow(headers)
         for row in data:
--- analysis/main.py
+++ analysis/main.py
@@ -3,24 +3,24 @@ from os.path import exists
 
 from etymology_tree_search import add_virtual_columns, populate_ety_depths, prepare_data
 from file_management import save_as_csv
-from config import prepare_globals, prepare_virtual_fields, root, paths, ety_depth
+from config import prepare_globals, prepare_virtual_fields, fill_clean_names, root, paths, ety_depth
 
 os.system('clear')
 
-
 # lines, element_hash_map, headers, header_hms = dataa[0], dataa[1], dataa[2], dataa[3]
 
-
 def __main__():
+    
     dataa = prepare_data(root, paths)
     add_virtual_columns(dataa, [ety_depth], ["-1"])
-    
+
     cs = prepare_globals(dataa)
     prepare_virtual_fields(dataa, cs)
-    
-    if not exists(f"{cs['root']}/temp_debug.txt"):  
+    fill_clean_names(dataa, cs)
+    if not exists(f"{cs['root']}/temp_debug.csv"):  
         save_as_csv(cs['root'], dataa[0], "debug", dataa[2])
         
+    exit()
     new_additives = populate_ety_depths(dataa, cs)
     
 __main__()
