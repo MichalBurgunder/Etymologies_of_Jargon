63b8f39fa3eddeb53bd591e88373b3a4133674d9
Michal Burgunder <michal_burgunder@yahoo.com>
2022-11-06 16:41:48 +0100

    jargons are now precomputed

--- analysis/config.py
+++ analysis/config.py
@@ -1,4 +1,4 @@
-jargon_entries = ["X1st Jargon", "X2nd Jargon", "X3rd Jargon", "X4th Jargon"] # the fields which will create our tree
+jargon_entries = ["1st Jargon", "2nd Jargon", "3rd Jargon", "4th Jargon"] # the fields which will create our tree
 root = '/Users/michal/Documents/thesis/etymologies_of_jargon/thesis_data'
 paths = [
             'programming_languages.csv', # programming languages
@@ -31,13 +31,27 @@ def find_field_position(headers, field):
 def prepare_virtual_fields(dataa, cs):
     cs['ety_depth_pos'] = find_field_position(dataa[2], cs['ety_depth'])
     cs['clean_name_pos'] = find_field_position(dataa[2], cs['clean_name'])
-    
+
+def find_jargon_entry_positions(headers, jargons):
+    entry_positions = []
+    for h in range(0, len(headers)):
+        for j in jargons:
+            if j == headers[h]:
+                entry_positions.append(h)
+                break
+
+    if len(entry_positions) != len(jargons):
+        print("cant find jargons")
+        exit()
+    return entry_positions
+
 def prepare_globals(dataa):
     global consts
     
     print("?")
     consts = {
-        'jargon_entries': ["X1st Jargon", "X2nd Jargon", "X3rd Jargon", "X4th Jargon"], # the fields which will create our tree
+        'jargon_entries': jargon_entries, # the fields which will create our tree
+        'jargon_entry_positions': None,
         'root': '/Users/michal/Documents/thesis/etymologies_of_jargon/thesis_data',
         'paths': [
                     'programming_languages.csv', # programming languages
@@ -53,6 +67,11 @@ def prepare_globals(dataa):
         'ety_depth_pos': None,
     }
 
+    # lets find the jargon entry positions
+    consts['jargon_entry_positions'] = find_jargon_entry_positions(dataa[2], consts['jargon_entries'])
+
+        
+        
     prepare_virtual_fields(dataa, consts)
 
     return consts
