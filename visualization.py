3c762c10c5fa339751060b0e3ad0db98b72ad725
Michal Burgunder <michal_burgunder@yahoo.com>
2022-11-28 15:11:33 +0100

    first acceptable visualization ety types by decade

--- visualization.py
+++ visualization.py
@@ -2,8 +2,14 @@ import csv
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-import sys        
+import sys  
+import random      
 sys.path.append('/Users/michal/Documents/thesis/etymologies_of_jargon/analysis')
+
+import matplotlib as mpl
+from matplotlib.colors import ListedColormap, LinearSegmentedColormap
+
+
 
 from analysis.file_management import read_csv
 # from analysis import config
@@ -63,18 +69,27 @@ def convert_to_ints(data):
     for i in range(0,len(data)):
         for j in range(0,len(data[i])):
             data[i][j] = int(data[i][j])
-    return data
-            
-def ety_types(filename):
-    data = read_csv(filename)
-    columns = data[0] # fetching columns
-    rows = data[-1] # fetching rows
-    data_stringed = data[1:len(data)-1] # clean data is left behind
-    data = convert_to_ints(data_stringed)
+    return np.array(data)
+        
+def order_data_by_frequency(data):
+    sorted_sums = sorted([(sum(data[i]), i) for i in range(0,len(data))])
+    sorted_sums.reverse()
+    
+    sorted_data = []
+    for i in range(0, len(data)):
+        sorted_data.append(data[sorted_sums[i][1]])
 
-    # Get some pastel shades for the colors
-    colors = plt.cm.BuPu(np.linspace(0, 0.5, len(rows)))
-    colors = plt.cm.tab20(np.linspace(0, 0.5, len(rows)))
+    return sorted_data
+    
+def ety_types(filename):
+    data_csv = read_csv(filename)
+    columns = data_csv[0] # fetching columns
+    rows = data_csv[-1] # fetching rows
+    data = data_csv[1:len(data_csv)-1]
+    data = convert_to_ints(data)
+    data = order_data_by_frequency(data)
+
+    colors = plt.cm.tab20((4./3*np.arange(len(rows))).astype(int))
 
     n_rows = len(data)
 
@@ -83,35 +98,38 @@ def ety_types(filename):
 
     # Initialize the vertical-offset for the stacked bar chart.
     # y_offset = np.ones(len(columns))
+   
 
+    fig, ax = plt.subplots(num=None, figsize=(16, 12), dpi=80, facecolor='w', edgecolor='k')
     # Plot bars and create text labels for the table
     cell_text = []
+    bottoms = None
     for row in range(0,n_rows):
-        # print(data[row])
-        plt.bar(index, data[row], width=bar_width, color=colors[row], align='center')
+        bottoms = [0]*len(columns) if row == 0 else bottoms + data[row-1]
+        plt.bar(index, data[row], width=bar_width, bottom=bottoms, color=colors[row], align='center')
         cell_text.append(data[row])
-    # Reverse colors and text labels to display the last value at the top.
-    colors = colors[::-1]
+        
+
+    
     plt.xticks(ticks=[])
     plt.yticks(ticks=range(0,30,5), labels=range(0,30,5)) #  labels=columns
-    # plt.show()
-    # exit()
-    # cell_text.reverse()
-    # print(colors)
-    # print(len(colors))
+
     # Add a table at the bottom of the axes
     the_table = plt.table(cellText=cell_text,
                           rowLabels=rows,
                           rowColours=colors,
                           colLabels=columns,
                           loc='bottom',
-                          colWidths=(0.072,)*len(columns),
+                          colWidths=(0.073,)*len(columns),
                         #   colWidths=[0.5 for i in n_rows],
                         colLoc='center'
                           )
+    # the_table.
+    # fig, ax = plt.subplots(num=None, figsize=(16, 12), dpi=80, facecolor='w', edgecolor='k')
     # [the_table.auto_set_font_size(False) for t in [tab1, tab2]]
     # Adjust layout to make room for the table:
     plt.subplots_adjust(left=0.2, bottom=0.5)
+   
     # plt.subplot(figsize=(16, 12))
 
     # plt.ylabel("Loss in ${0}'s".format(value_increment))
