b30d9b30e98f8b480d6119e69339ed03ced52d0e
Michal Burgunder <michal_burgunder@yahoo.com>
2022-12-15 12:09:20 +0100

    script to sample ruby gems

---
+++ analysis/sample_gems.py
@@ -0,0 +1,28 @@
+import random
+import pandas as pd
+from file_management import save_as_csv
+from config import raw_data_root, root
+
+def sample_from_csv(file_name, resulting_file_name):
+    final_data = []
+    # df = pd.read_csv(f"{root}/{resulting_file_name}.csv")
+    df = pd.read_csv(f"{raw_data_root}/{file_name}.csv")
+    number_rows = len(df.axes[0])
+    random_numbers = [random.randrange(0, number_rows) for i in range(0,100)]
+    
+    for i in range(len(df)):
+        if i in random_numbers:
+            # lets automatically also clean the data
+            word = ""
+            for j in range(0,len(df.iloc[i, 0])):
+                # print(df.iloc[i, 0][j])
+                if df.iloc[i, 0][j] != "(":
+                    word += df.iloc[i, 0][j]
+                else:  
+                    break
+            final_data.append([word])
+
+    save_as_csv(final_data, "sampled_gems", False, {"subfolder": "raw_data"})
+    return f"{root}/{resulting_file_name}.csv"
+
+sample_from_csv('gems', 'sampled_gems')
