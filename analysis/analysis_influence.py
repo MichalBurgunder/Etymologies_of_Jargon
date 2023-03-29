026d85518d9691a7b0277fe2602bf3e4e9c583ac
Michal Burgunder <michal_burgunder@yahoo.com>
2023-03-29 16:10:31 -0400

    forgot to insert code

--- analysis/analysis_influence.py
+++ analysis/analysis_influence.py
@@ -1,5 +1,5 @@
 import numpy as np
-import 
+# import 
 
 def pagerank(M, num_iterations: int = 100, d: float = 0.85):
     '''
@@ -27,3 +27,30 @@ def create_pagerank_matrix(data, elem_entry_hm, cs):
             the_matrix[i][pos] = the_num
             
         pagerank(the_matrix)
+        
+
+def recursive_influ(matrix, resulting_matrix, i, adder=0):
+    # additional_here = 0
+    for j in range(0,len(matrix[i])):
+        if matrix[i][j] == 1:
+            resulting_matrix[j] += 1
+            adder += 1
+            recursive_influ(matrix, resulting_matrix, j, adder)
+            # print(i, additional)
+            # additional_here += additional
+    # print(additional_here)
+    resulting_matrix[i] += adder
+    # print(str(resulting_matrix))
+    
+    # return additional_here + 1
+    return 0
+    
+def influence(matrix):
+    n = matrix.shape[0]
+    resulting_matrix = np.zeros(n)
+    # arrival_matrix = np.zeros((n,n))
+    # print(" A  B  C  D  E  F  G  H")
+    for i in range(0,len(matrix)):
+        recursive_influ(matrix, resulting_matrix, i) 
+        
+    return resulting_matrix
