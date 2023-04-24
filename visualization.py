55d46ea299e91fa4916e480a338d5bf1f3024582
Michal Burgunder <michal_burgunder@yahoo.com>
2023-04-24 12:51:12 +0200

    working full visualization fiel pre-refactor

--- visualization.py
+++ visualization.py
@@ -222,7 +222,7 @@ def get_bargraph_data(data_set, path):
     """
     Extracts the data of a specified .csv file (path), from a specific data set (set)
     """
-    path = file_names[path]+data_set
+    path = file_names[path]+ "_" + data_set
     data = read_csv(path)
     return [data[0], [int(data[1][j]) for j in range(0, len(data[0]))] ]
 
@@ -488,39 +488,39 @@ def ety_types_bar_graph():
 # NUMBER OF CHARACTERS
 
 # number of characters per set
-# bar_graphs_characters("ALL")
-# bar_graphs_characters("PL")
-# bar_graphs_characters("CP")
-# bar_graphs_characters("RG")
-# bar_graphs_characters("PM")
+bar_graphs_characters("ALL")
+bar_graphs_characters("PL")
+bar_graphs_characters("CP")
+bar_graphs_characters("RG")
+bar_graphs_characters("PM")
 
 # bar_graphs_characters_by_year()
 
-# noc_all = stats_on_numbers("All", "name_length", "Number of Characters")
-# noc_pl = stats_on_numbers("PL", "name_length", "Number of Characters")
-# noc_cp = stats_on_numbers("CP", "name_length", "Number of Characters")
-# noc_rg = stats_on_numbers("RG", "name_length", "Number of Characters")
-# noc_pm = stats_on_numbers("PM", "name_length", "Number of Characters")
+noc_all = stats_on_numbers("All", "name_length", "Number of Characters")
+noc_pl = stats_on_numbers("PL", "name_length", "Number of Characters")
+noc_cp = stats_on_numbers("CP", "name_length", "Number of Characters")
+noc_rg = stats_on_numbers("RG", "name_length", "Number of Characters")
+noc_pm = stats_on_numbers("PM", "name_length", "Number of Characters")
 
 
 # NUMBER OF MORPHEMES
 
 # number morphemes per set
-# bar_graphs_morphemes("ALL")
-# bar_graphs_morphemes("PL")
-# bar_graphs_morphemes("CP")
-# bar_graphs_morphemes("RG")
-# bar_graphs_morphemes("PM")
+bar_graphs_morphemes("ALL")
+bar_graphs_morphemes("PL")
+bar_graphs_morphemes("CP")
+bar_graphs_morphemes("RG")
+bar_graphs_morphemes("PM")
 
 
 # statistical data on mophemes per set
-# nom_all = stats_on_numbers("All", "morpheme", "Number of Morphemes")
-# nom_pl = stats_on_numbers("PL", "morpheme", "Number of Morphemes")
-# nom_cp = stats_on_numbers("CP", "morpheme", "Number of Morphemes")
-# nom_rg =  stats_on_numbers("RG", "morpheme", "Number of Morphemes")
-# nom_pm = stats_on_numbers("PM", "morpheme", "Number of Morphemes")
-# nom_top = stats_on_numbers("TOP", "morpheme", "Number of Morphemes")
-# print_length_stats_latex([nom_all, nom_pl, nom_cp, nom_rg, nom_pm, nom_top], ["All", "PL", "CP", "RG", "PM", "TOP"])
+nom_all = stats_on_numbers("All", "morpheme", "Number of Morphemes")
+nom_pl = stats_on_numbers("PL", "morpheme", "Number of Morphemes")
+nom_cp = stats_on_numbers("CP", "morpheme", "Number of Morphemes")
+nom_rg =  stats_on_numbers("RG", "morpheme", "Number of Morphemes")
+nom_pm = stats_on_numbers("PM", "morpheme", "Number of Morphemes")
+nom_top = stats_on_numbers("TOP", "morpheme", "Number of Morphemes")
+print_length_stats_latex([nom_all, nom_pl, nom_cp, nom_rg, nom_pm, nom_top], ["All", "PL", "CP", "RG", "PM", "TOP"])
 
 # etymological depths
 number_of_depths("ety_depths") # defaults to all
@@ -530,17 +530,17 @@ number_of_depths("ety_depths", "RG")
 number_of_depths("ety_depths", "PM")
 
 # ety types per decade
-# ety_types('ety_type_1_by_decade')
-# ety_types('ety_type_2_by_decade')
+ety_types('ety_type_1_by_decade')
+ety_types('ety_type_2_by_decade')
 
 # stats on ety types
-# ety_types_table_pl('ety_type_2_by_decade')
-# ety_types_bar_graph()
-# ety_types('ety_type_2_by_decade', normalized=True)
-# ety_types_table_pl('ety_type_2_by_decade')
+ety_types_table_pl('ety_type_2_by_decade')
+ety_types_bar_graph()
+ety_types('ety_type_2_by_decade', normalized=True)
+ety_types_table_pl('ety_type_2_by_decade')
 
 # cultural heritage
-# bar_graphs_ch() # "CH", 'cultural_heritage'
+bar_graphs_ch() # "CH", 'cultural_heritage'
 
 # version numbering
-# version_numbering("All")
+version_numbering("All")
