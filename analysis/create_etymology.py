c5af80d87bd2a9447483c327922bae22214aa600
Michal Burgunder <michal_burgunder@yahoo.com>
2023-06-22 07:58:10 +0200

    a 5min apop that allows you to query etymologies

--- analysis/create_etymology.py
+++ analysis/create_etymology.py
@@ -3,15 +3,23 @@ from config import jargon_entries
 
 unallowed = ["gnu", "pip"]
 
-def create_etymology(data, hm, name, headers, iteration_num=1):
+def create_etymology(data, hm, name, headers, iteration_num=1, simple=True):
     """
     basic etymology creator. Hi audience!
     """
     name_pos = hm[name]
+    explanation = find_field_position(headers, "Explanation")
+    
+    if not simple:
+        print("-- "*iteration_num + data[name_pos][explanation])
     for j in jargon_entries:
         j_pos = find_field_position(headers, j)
         if data[name_pos][j_pos] != "" and data[name_pos][j_pos] not in unallowed:
             print("-- "*iteration_num + data[name_pos][j_pos])
+
+            
             create_etymology(data, hm, data[name_pos][j_pos], headers, iteration_num+1)
+    # if not intermediary:
+    
     return 
     
